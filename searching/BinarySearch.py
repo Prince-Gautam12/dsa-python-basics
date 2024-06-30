a912179940df24930caabc14696a9f4913a9e860
akshaygautam <akshay.gautam2209@gmail.com>
2024-06-30 16:14:16 +0530

    binary search complete for deacreasing also

--- searching/BinarySearch.py
+++ searching/BinarySearch.py
@@ -13,6 +13,24 @@ class BinarySearch:
                 right = mid-1
         return False
     
+    def decresingSearch(self,array,number):
+        l = len(array)
+        left = 0
+        right = l-1
+        while left <= right:
+            mid = (right+left)//2
+            if number == array[mid]:
+                return True
+            if number > array[mid]:
+                right = mid - 1
+            if number < array[mid]:
+                left = mid + 1
+        return False
+
+    
 searching = BinarySearch()
 found = searching.search([1,2,3,4,5,6,7,8,9],8)
+print(found)
+
+found = searching.decresingSearch([9,8,7,6,5,4,3,2,1],7)
 print(found)
