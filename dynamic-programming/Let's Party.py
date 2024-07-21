87abe53cfd9f2b6291496ee269d27c81c5d19709
Prince-Gautam12 <princegautam2623@gnail.com>
2024-07-21 20:52:17 +0530

    Let's Party

---
+++ dynamic-programming/Let's Party.py
@@ -0,0 +1,44 @@
+class Solution:
+    def party(self, A):
+        if (A== 1 or A == 2):
+            return A
+        return ((self.party(A-1)) % 10003 + ((A-1) * self.party(A-2)) % 10003) % 10003
+
+
+    def solve(self, A):
+        if (A == 1 or A == 2):
+            return A
+        def calc(A, cache):
+            if (cache[A] != None): return cache[A]
+            ans = (calc(A-1, cache) % 10003 + ((A-1) * calc(A-2, cache)) % 10003) % 10003
+            cache[A] = ans
+            return ans
+        
+        cache=[None]*(A+1)
+        cache[1] = 1
+        cache[2] = 2
+        return calc(A, cache)
+    
+    def partyTab(self, A):
+        if (A == 1 or A == 2):
+            return A
+        cache = [None]*(A+1)
+        cache[1] = 1
+        cache[2] = 2
+        for i in range(3, A+1):
+            ans = ((cache[i - 1]) % 10003 + ((i-1) * cache[i - 2]) % 10003) % 10003
+            cache[i] = ans
+        return cache[A]
+    
+    
+
+
+solution = Solution()
+answer = solution.solve(3)
+print(answer) 
+
+answer = solution.partyTab(5)
+print(answer)
+
+# answer = solution.solve(60041)
+# print(answer)
